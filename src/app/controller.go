3c6b8479415e038094747f6a103ccd2faeef7470
OBAID ULLAH KHAN <obaidullahkhankhail@gmail.com>
2022-11-10 13:26:25 +0500

    data validation

--- src/app/controller.go
+++ src/app/controller.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -34,45 +33,84 @@ func registerRoutes() *gin.Engine {
 
 	})
 
+	r.POST("/employees/:id/vacation/new", func(ctx *gin.Context) {
+		var timeOff TimeOff
+		err := ctx.BindJSON(&timeOff)
+
+		if err != nil {
+			ctx.String(http.StatusBadRequest, err.Error())
+			return
+		}
+
+		id := ctx.Param("id")
+
+		timesOff, ok := TimesOff[id]
+
+		if !ok {
+			TimesOff[id] = []TimeOff{}
+		}
+
+		TimesOff[id] = append(timesOff, timeOff)
+	})
+
 	admin := r.Group("/admin", gin.BasicAuth(gin.Accounts{
 		"admin": "admin",
 	}))
 	admin.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(http.StatusOK, "admin-overview.html", nil)
+		ctx.HTML(http.StatusOK, "admin-overview.html",
+			map[string]interface{}{
+				"Employess": employees,
+			})
+	})
+
+	admin.GET("/employees/:id", func(ctx *gin.Context) {
+		id := ctx.Param("id")
+		if id == "add" {
+			ctx.HTML(http.StatusOK, "admin-employee-add.html", nil)
+			return
+		}
+		employee, ok := employees[id]
+		if !ok {
+			ctx.String(http.StatusNotFound, "404 - Not Found")
+			return
+		}
+		ctx.HTML(http.StatusOK, "admin-employee-edit.html",
+			map[string]interface{}{
+				"Employee": employee,
+			})
 	})
 
 	admin.POST("/employees/:id", func(ctx *gin.Context) {
 		id := ctx.Param("id")
 		if id == "add" {
-			pto, err := strconv.ParseFloat(ctx.PostForm("pto"), 32)
-			if err != nil {
-				ctx.String(http.StatusBadRequest, err.Error())
-				return
-			}
+			// pto, err := strconv.ParseFloat(ctx.PostForm("pto"), 32)
+			// if err != nil {
+			// 	ctx.String(http.StatusBadRequest, err.Error())
+			// 	return
+			// }
 
-			startDate, err := time.Parse("2006-01-02", ctx.PostForm("pto"))
+			startDate, err := time.Parse("2006-01-02", ctx.PostForm("startDate"))
 			if err != nil {
 				ctx.String(http.StatusBadRequest, err.Error())
 				return
 			}
 
 			var emp Employee
+			err = ctx.Bind(&emp)
+			if err != nil {
+				ctx.String(http.StatusBadRequest, err.Error())
+				return
+			}
 			emp.ID = 42
-			emp.FirstName = ctx.PostForm("firstName")
-			emp.LastName = ctx.PostForm("lastNAme")
-			emp.Position = ctx.PostForm("position")
 			emp.Status = "Active"
-			emp.TotalPTO = float32(pto)
 			emp.StartDate = startDate
 			employees["42"] = emp
 
 			ctx.Redirect(http.StatusMovedPermanently, "/admin/employees/42")
-
 		}
 	})
 
 	r.Static("/public", "./public")
 	// r.StaticFS("/public", http.Dir("./public"))
-
 	return r
 }
